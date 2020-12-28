cac053b06f6e3ef5802bc8327be656e5932161af
patryckpinhal <48493265+patryckpinhal@users.noreply.github.com>
2020-12-28 08:39:12 -0300

    Update banidos.sh

--- banidos.sh
+++ banidos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+iptables -I INPUT -s 149.202.202.23 -j DROP
 iptables -I INPUT -s 51.81.152.33 -j DROP
 iptables -I INPUT -s 193.32.8.15 -j DROP
 iptables -I INPUT -s 20.55.194.245 -j DROP
