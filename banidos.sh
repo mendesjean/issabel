c2ae8edc4c07224ee6a6789e26f8bef5dbe59ab0
Patryck Pinhal <48493265+patryckpinhal@users.noreply.github.com>
2022-05-02 17:18:07 -0300

    Update banidos.sh

--- banidos.sh
+++ banidos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+iptables -I INPUT -s 159.65.127.62 -j DROP
 iptables -I INPUT -s 51.91.29.0/24 -j DROP
 iptables -I INPUT -s 141.94.139.30 -j DROP
 iptables -I INPUT -s 40.85.227.80 -j DROP
